cff66e9eb307f300cf432ad04e4fa6a8035af565
Adnan Sharief <adnanshareef700@gmail.com>
2023-08-23 21:19:06 +0530

    commit 3

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlchemy
+db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
@@ -16,4 +17,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note  
+
+    with app.app_context():
+        db.create_all()
+
     return app
+
+def create_databse(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
